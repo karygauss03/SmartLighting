643311237b73e8c3b96819013f0f7e1bd4f45175
karygauss03 <karim.omrane@supcom.tn>
2022-12-16 14:41:46 +0100

    Updating city entity

--- smart_lighting/src/main/java/tn/cot/smartlighting/entities/City.java
+++ smart_lighting/src/main/java/tn/cot/smartlighting/entities/City.java
@@ -9,37 +9,27 @@ import javax.json.bind.annotation.JsonbVisibility;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.Objects;
-import java.util.UUID;
 
 @Entity
 @JsonbVisibility(FieldPropertyVisibilityStrategy.class)
 @Table(name = "cities")
 public class City {
     @Id
-    private UUID id;
+    private String id;
     @Column
     private Country country;
     @Column
     private String name;
-    @Column
-    private LocalDateTime createdOn;
 
     public City() { };
-    public City(Country country, String name) {
-        this.id = UUID.randomUUID();
+    public City(Country country, String name, LocalDateTime dateTime) {
         this.country = country;
         this.name = name;
-        this.createdOn = LocalDateTime.now();
-    }
-
-
-    public String getCityName() {
-        return this.name;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, country, name, createdOn);
+        return Objects.hash(id, country, name);
     }
 
     @Override
@@ -47,6 +37,15 @@ public class City {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         City city = (City) o;
-        return Objects.equals(id, city.id) && country == city.country && Objects.equals(name, city.name) && Objects.equals(createdOn, city.createdOn);
+        return Objects.equals(id, city.id) && country == city.country && Objects.equals(name, city.name);
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id='" + id + '\'' +
+                ", country=" + country +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
