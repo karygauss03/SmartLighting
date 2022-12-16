1b12982a94a7de6ccd1e458283b2bf95e3b31d68
karygauss03 <karim.omrane@supcom.tn>
2022-12-16 09:57:24 +0100

    Updating template + adding mongodb connection

--- smart_lighting/src/main/java/tn/smart_lighting/JaxRsActivator.java
+++
@@ -1,8 +0,0 @@
-package tn.smart_lighting;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("rest-api")
-public class JaxRsActivator extends Application {
-}
