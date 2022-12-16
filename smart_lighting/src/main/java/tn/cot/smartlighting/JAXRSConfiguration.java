a4f590f8397aa98d42df6a2fc799e740a658ab16
karygauss03 <karim.omrane@supcom.tn>
2022-12-16 10:01:11 +0100

    update

--- smart_lighting/src/main/java/tn/cot/smartlighting/JAXRSConfiguration.java
+++ smart_lighting/src/main/java/tn/cot/smartlighting/JAXRSConfiguration.java
@@ -3,10 +3,6 @@ package tn.cot.smartlighting;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/**
- * Configures JAX-RS for the application.
- * @author Adam M. Gamboa G
- */
 @ApplicationPath("api")
 public class JAXRSConfiguration extends Application {
     
