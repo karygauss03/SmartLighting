573f84b9d57bf467f1e271d379ca528c64401891
karygauss03 <karim.omrane@supcom.tn>
2022-12-17 10:55:15 +0100

    Adding address repository

---
+++ smart_lighting/src/main/java/tn/cot/smartlighting/repositories/AddressRepository.java
@@ -0,0 +1,13 @@
+package tn.cot.smartlighting.repositories;
+
+import jakarta.nosql.mapping.Repository;
+import tn.cot.smartlighting.entities.Address;
+
+import java.util.stream.Stream;
+
+public interface AddressRepository extends Repository<Address, String> {
+    Stream<Address> findAll();
+    Address findById();
+    Stream<Address> findByCity();
+    Stream<Address> findByCountry();
+}
