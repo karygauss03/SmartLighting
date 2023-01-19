0c5619311665141f56a8fecd4e5e9e781a0ca395
karygauss03 <karim.omrane@supcom.tn>
2023-01-19 21:03:19 +0100

    Addig MQTT Connection

--- smart_lighting/src/main/java/tn/cot/smartlighting/mqtt/MqttConnection.java
+++ smart_lighting/src/main/java/tn/cot/smartlighting/mqtt/MqttConnection.java
@@ -2,14 +2,19 @@ package tn.cot.smartlighting.mqtt;
 
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
-import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import tn.cot.smartlighting.repositories.LightingModuleRepository;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 import javax.inject.Inject;
+import javax.net.ssl.SSLSocketFactory;
+import javax.sql.ConnectionEventListener;
 
 @Singleton
 @Startup
@@ -27,5 +32,56 @@ public class MqttConnection {
         client.publish(topic,message);
     }
 
+    @PostConstruct
+    public void start() {
+        try {
+            System.out.println("\n **************** \n");
+            System.out.println("MQTT CONNECTED");
+            System.out.println("\n **************** \n");
 
+            MqttClient client = new MqttClient(
+                    uri,
+                    MqttClient.generateClientId(),
+                    new MemoryPersistence());
+
+            MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+            mqttConnectOptions.setUserName(username);
+            mqttConnectOptions.setPassword(password.toCharArray());
+            mqttConnectOptions.setConnectionTimeout(0);
+            mqttConnectOptions.setSocketFactory(SSLSocketFactory.getDefault());
+            client.connect(mqttConnectOptions);
+            client.setCallback(new MqttCallback() {
+                @Override
+                public void connectionLost(Throwable throwable) {
+                    System.out.println("\n **************** \n");
+                    System.out.println("MQTT LOST CONNECTION");
+                    System.out.println("\n **************** \n");
+                }
+
+                @Override
+                public void messageArrived(String topic, MqttMessage mqttMessage) {
+                    System.out.println("MESSAGE RECEIVED");
+                    System.out.println("\n **************** \n");
+                    System.out.println(topic);
+                    System.out.println("\n **************** \n");
+
+                    if (topic.equals("test")) {
+                        System.out.println("Topic " + topic + " well received");
+                        System.out.println("Message " + mqttMessage);
+                    }
+                }
+
+                @Override
+                public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+                    System.out.println("\n **************** \n");
+                    System.out.println("delivery complete " + iMqttDeliveryToken);
+                }
+            });
+
+            client.subscribe("test");
+        }
+        catch (MqttException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
